a84446f7e1baee32f022ae15f2d05c8ee168ff9b
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-15 06:10:50 +0000

    customize brand font

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,4 @@
-import { Fira_Code as FontMono, DM_Sans as FontSans } from "next/font/google";
+import { Fira_Code as FontMono, Teachers as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
