8036843735d1fd9532ca046dee9c53b1fca6d371
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-15 06:09:15 +0000

    make the background pokemon subtle doodle

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="py-4"></div>
         <Search />
       </div>
-      <div className="container bg-red-100 grid p-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
+      <div className="container grid p-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
         <SinglePokemon />
         <SinglePokemon />
         <SinglePokemon />
